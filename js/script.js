00692720744420792101110dcb85932d4e95573e
HectorMario <hector.mario.peraz@gmail.com>
2023-03-16 22:50:09 +0100

    sensa style

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+function palin(lopo){
+    let largo = lopo.length;
+    let valu = [];
+    for (let i = 0; i < largo; i++){
+        let xd = false;
+        if(lopo[i] == lopo[largo -1 - i]){
+            xd = true;
+        }
+        valu.push(xd)
+    }
+    console.log(valu);
+    
+    let yuca = true;
+    let i = 0
+    while (i < largo && yuca !== false){
+        if(valu[i] ==  false){
+            yuca = false;
+        } 
+        i++
+    }
+
+    if(yuca == true){
+        alert("è palindroma")
+    } else{
+        alert("non è palindroma")
+    }
+}
+
+function num (){
+    let pc = Math.ceil(Math.random()*5);
+    let userNum = prompt("dimi un numero tra 1 e 5")
+
+    let resultUser = prompt("La risposta es pari o dispari?")
+
+    let sum = pc + userNum;
+
+    if(sum % 2 == 0 && resultUser == "pari"){
+        alert("hai vinto")
+    }else{
+        alert("hai perso")
+    }
+}
+
+
+palin("non");
+num();
